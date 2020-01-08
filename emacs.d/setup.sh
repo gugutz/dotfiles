d76533b0a51c5da23b1ad8a39090cb22c408155e
Gustavo P Borges <gugutz@gmail.com>
2020-01-08 17:54:13 -0300

    sync

--- emacs.d/setup.sh
+++ emacs.d/setup.sh
@@ -12,7 +12,7 @@ echo "install ttf-all-the-icons from aur"
 echo "this cant be `--noconfirm` because it replaces a lib and requires confirmation"
 yay -S ttf-all-the-icons
 
-echo "## install pandoc for document generation in org mode"
+echo "## all the icons"
 yay -S emacs-all-the-icons
 
 echo "install ag searcher for dumb-jump to work better"
@@ -21,14 +21,19 @@ sudo pacman -S --noconfirm the_silver_searcher
 echo "## install typescript/javascript language server"
 npm install -g typescript-language-server
 
+echo "## install vscode-html-languageserver"
+npm i -g vscode-html-languageserver-bin
+
 echo "## install eslint"
 npm install -g eslint babel-eslint eslint-plugin-react
 echo "## install eslint typescript plugin"
 npm install -g @typescript-eslint/eslint-plugin@latest
 
-echo "## install angular language server3"
-wget -o angular-language-server.zip https://marketplace.visualstudio.com/_apis/public/gallery/publishers/Angular/vsextensions/ng-template/0.802.3/vspackage
-tar -xvf angular-language-server --directory ~/.angular
+echo "## install angular language server"
+npm install -g @angular/language-service@next typescript  @angular/language-server
+
+echo "## install css language server"
+npm install --global vscode-css-languageserver-bin
 
 echo "## install pandoc for document generation in org mode"
 sudo pacman -S --noconfirm pandoc
