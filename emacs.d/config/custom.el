5effee38993cea70a70da1b9416d3b8f91a4a723
gugutz <gugutz@gmail.com>
2020-01-26 21:37:49 -0300

    sync

---
+++ emacs.d/config/custom.el
@@ -0,0 +1,58 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-echo-delay 0 t)
+ '(company-idle-delay 0.1)
+ '(diff-hl-fringe-bmp-function (quote diff-hl-fringe-bmp-from-type))
+ '(diff-hl-margin-side (quote left))
+ '(diff-hl-side (quote right))
+ '(dumb-jump-selector (quote ivy) t)
+ '(evil-emacs-state-cursor (quote ("#ff0000" box)) t)
+ '(evil-insert-state-cursor (quote ("#e2f00f" bar)) t)
+ '(evil-mode-line-format (quote (before . mode-line-front-space)))
+ '(evil-motion-state-cursor (quote ("#FFFFFF" box)) t)
+ '(evil-normal-state-cursor (quote ("#00ff00" box)) t)
+ '(evil-operator-state-cursor (quote ("red" hollow)) t)
+ '(evil-replace-state-cursor (quote ("red" hbar)) t)
+ '(evil-visual-state-cursor (quote ("#abcdef" box)) t)
+ '(eyebrowse-mode t)
+ '(highlight-indent-guides-auto-enabled t)
+ '(highlight-indent-guides-method (quote character))
+ '(highlight-indent-guides-responsive t)
+ '(lsp-prefer-flymake nil)
+ '(package-selected-packages
+   (quote
+    (web-mode editorconfig flycheck-posframe flycheck expand-region parrot minions format-all centaur-tabs which-key emmet-mode prettier-js js2-refactor scss-mode yasnippet-snippets vi-tilde-fringe use-package typescript-mode treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil shell-pop rotate rainbow-mode rainbow-delimiters move-text ivy-rich ivy-prescient ivy-posframe ivy-explorer hl-todo highlight-parentheses highlight-operators highlight-numbers highlight-indent-guides highlight-escape-sequences helpful goto-line-preview eyebrowse evil-commentary dumb-jump drag-stuff dired-k diminish diff-hl counsel company-lsp company-box color-identifiers-mode amx all-the-icons-ivy all-the-icons-dired)))
+ '(prettier-js-show-errors (quote echo) t)
+ '(projectile-completion-system (quote ivy) t)
+ '(show-paren-delay 0.4)
+ '(show-paren-highlight-openparen t)
+ '(show-paren-style (quote mixed))
+ '(show-paren-when-point-in-periphery t)
+ '(show-paren-when-point-inside-paren t)
+ '(web-mode-code-indent-offset 2 t)
+ '(web-mode-css-indent-offset 2 t)
+ '(web-mode-enable-auto-pairing t t)
+ '(web-mode-enable-block-face t t)
+ '(web-mode-enable-current-element-highlight t t)
+ '(web-mode-enable-part-face t t)
+ '(web-mode-markup-indent-offset 2 t)
+ '(which-key-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:height 4.0 :foreground "#f1fa8c"))))
+ '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
+ '(diff-hl-change ((t (:foreground "#f4f4f4"))))
+ '(diff-hl-delete ((t (:background "#ff3300" :foreground "#ffffff"))))
+ '(diff-hl-insert ((t (:background "#00ff00"))))
+ '(font-lock-comment-face ((t (:foreground "#828282"))))
+ '(ivy-posframe ((t (:background "#333244"))))
+ '(ivy-posframe-border ((t (:background "#abff00"))))
+ '(ivy-posframe-cursor ((t (:background "#00ff00"))))
+ '(show-paren-match ((nil (:background "#0a71d0" :foreground "#ffffff"))))
+ '(show-paren-mismatch ((nil (:background "red" :foreground "black")))))
